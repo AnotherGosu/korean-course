1c90235995553c65a2209496f905ca328b852d06
Alex Boot <a.but@mxmgt.com>
2022-05-22 19:06:05 +0900

    Mobile viewport height fix

--- components/sections/Home/index.js
+++ components/sections/Home/index.js
@@ -1,26 +1,11 @@
 import styles from "./Home.module.css";
 import Image from "next/image";
-import { useState, useEffect } from "react";
 
 import Polaroid from "../../common/Polaroid";
 
 export default function Home() {
-  const [vh, setVh] = useState("");
-
-  useEffect(() => {
-    const changeVh = () => {
-      if (window) {
-        const innerVh = window.innerHeight * 0.01;
-        setVh(`${innerVh}px`);
-      }
-    };
-
-    window.addEventListener("resize", changeVh);
-    return () => window.removeEventListener("resize", changeVh);
-  }, []);
-
   return (
-    <section className={styles.container} style={{ "--vh": vh }}>
+    <section className={styles.container}>
       <Polaroid>
         <Image
           src="/polaroid.jpg"
