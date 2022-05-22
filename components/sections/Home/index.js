35bb6c12c8e0da80a7406fb379156e0fe95d7b8d
Alex Boot <a.but@mxmgt.com>
2022-05-22 19:27:44 +0900

    Commit

--- components/sections/Home/index.js
+++ components/sections/Home/index.js
@@ -6,16 +6,18 @@ import Polaroid from "../../common/Polaroid";
 export default function Home() {
   return (
     <section className={styles.container}>
-      <Polaroid>
-        <Image
-          src="/polaroid.jpg"
-          alt="Мари"
-          width="960"
-          height="1280"
-          layout="intrinsic"
-        />
-        <span className={styles.title}>Maryoonseul</span>
-      </Polaroid>
+      <div className={styles.polaroidWrapper}>
+        <Polaroid>
+          <Image
+            src="/polaroid.jpg"
+            alt="Мари"
+            width="960"
+            height="1280"
+            layout="intrinsic"
+          />
+          <span className={styles.title}>Maryoonseul</span>
+        </Polaroid>
+      </div>
       <h1 className={styles.heading}>
         <div className={styles.icon} />
         <span>
